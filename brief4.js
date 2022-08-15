89a18945a0e393a0ff828b236144996a6bedd3b7
rababyoucode <110168436+rababyoucode@users.noreply.github.com>
2022-08-15 16:32:11 +0100

    secandcommet

--- brief4.js
+++ brief4.js
@@ -1,3 +1,3 @@
 let time = new Date();
-document.getelementByID("pdate").innerHTML= "La date est est: "+time.getFullYear();
+document.getElementById("pdate").innerHTML = "Les geste barrière peuvent sauver des vie " + time.getFullYear();
 
